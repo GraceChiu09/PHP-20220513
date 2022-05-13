3d8fa22fe1094c09f06a816f5fb7667cbe927e8d
gracechiu <rick.grace.1014@gmail.com>
2022-05-13 12:04:19 +0800

    增加判斷式

--- phplesson20220513/result.php
+++ phplesson20220513/result.php
@@ -25,22 +25,38 @@
     <?php
     $bmi="18";
    
-    if($bmi<18.5){
-        echo "體重過輕";
+    // if($bmi<18.5){
+    //     echo "體重過輕";
        
+    // }elseif($bmi>=24 || $bmi>=27){
+    //     echo "過重";
+    // }elseif($bmi>=27 || $bmi>=30){
+    //     echo "輕度肥胖";
+    // }elseif($bmi>=30 || $bmi>=35){
+    //     echo "中度肥胖";
+    // }else{
+    //     echo "正常範圍";
+    // }
 
-    }elseif($bmi>=24 || $bmi>=27){
-        echo "過重";
-    }elseif($bmi>=27 || $bmi>=30){
-        echo "輕度肥胖";
-    }elseif($bmi>=30 || $bmi>=35){
-        echo "中度肥胖";
-    }else{
+    if ($bmi < 18.5) {
+        echo "體重過輕";
+    } else if($bmi >= 18.5 && $bmi < 24){
         echo "正常範圍";
+    }else if($bmi >= 24 && $bmi < 27){
+        echo "過重";
+    }else if($bmi >= 27 && $bmi < 30){
+        echo "輕度肥胖";
+    }else if($bmi >= 30 && $bmi < 35){
+        echo "中度肥胖";
+    }else if($bmi > 35){
+        echo "重度肥胖";
     }
     
+
+?>
     
-    ?>
+    
+
     <div style="background-color:aquamarine">
     <a href="./bmi.html"><button>回到BMI計算</button></a>
 
